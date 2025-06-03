35c648bcdecb4ea8fab5faf5f126d5aeea25c039
toyxyz <toyxyz@naver.com>
2025-06-03 20:28:54 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,16 @@
-from .openpose_editor_nodes import OpenposeEditorNode
+from .openpose_editor_nodes import OpenposeEditorNode, PoseToMaskNode
 
 
 WEB_DIRECTORY = "js"
 
 NODE_CLASS_MAPPINGS = {
     "OpenposeEditorNode": OpenposeEditorNode,
+    "PoseToMaskNode": PoseToMaskNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "OpenposeEditorNode": "Openpose Editor Node",
+    "PoseToMaskNode": "Pose Keypoints to Mask",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
